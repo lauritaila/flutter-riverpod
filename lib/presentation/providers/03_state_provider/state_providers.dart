b94188b2724c724e2c186d28d61f4fd9ee8bbf64
Laura Avila <lavila@bigwise.com>
2025-06-23 16:46:16 -0400

    FEAT: implement state provider

---
+++ lib/presentation/providers/03_state_provider/state_providers.dart
@@ -0,0 +1,12 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'state_providers.g.dart';
+
+@riverpod
+class Counter extends _$Counter {
+  @override
+  int build() => 5;
+
+  void increment() => state++;
+  
+}
---
+++ lib/presentation/providers/03_state_provider/state_providers.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'state_providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$counterHash() => r'843c97cd521161c88c1bdbc7fedfb06545aa2a58';
+
+/// See also [Counter].
+@ProviderFor(Counter)
+final counterProvider = AutoDisposeNotifierProvider<Counter, int>.internal(
+  Counter.new,
+  name: r'counterProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$counterHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$Counter = AutoDisposeNotifier<int>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
